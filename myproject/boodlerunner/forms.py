ff7931156fd59584f1988c1e67017c349be6e622
AriMunkhtur <35264437+AriMunkhtur@users.noreply.github.com>
2018-03-26 11:44:49 -0400

    Add files via upload

---
+++ myproject/boodlerunner/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+#class ReceiverForm(forms.Form):
+#	name = forms.Charfield(label='Name', max_length=100)
+#	phoneNumber = forms.DecimalField(label='phone', max_digits=10)
+#	barracks = forms.Charfield(label='barracks', max_length=50)
+#	roomNumber = forms.DecimalField(label='room', max_digits=10)
+#	restaurant = forms.Charfield(label='restaurant', max_length=50)
+#	timeOfArrival = forms.DecimalField(label='time', max_digits=10)
+#	locationOfPickup = forms.Charfield(label='Location', max_length=100)
+#	AdditionalInstruction = forms.Charfield(label='AI', max_length=255)
