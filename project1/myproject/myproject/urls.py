47bb98c68551ea58302533ca135e3e4b9b1cf968
AriMunkhtur <35264437+AriMunkhtur@users.noreply.github.com>
2018-04-03 21:33:05 -0400

    Add files via upload

--- project1/myproject/myproject/urls.py
+++ project1/myproject/myproject/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'', include('boodlerunner.urls'))
+    url(r'', include('boodlerunner.urls')),
+
 ]
