16e4c79031e5b1b605fe2292b038573194f27984
AriMunkhtur <35264437+AriMunkhtur@users.noreply.github.com>
2018-04-18 23:21:09 -0400

    updated test.py
    initial test plan

--- project1/myproject/boodlerunner/tests.py
+++ project1/myproject/boodlerunner/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
 # Create your tests here.
+#Initial Test Plan:
+#We plan implementing three modes of testing. 
+############################################
+#First, is the single component test. This test will consist of breaking our application into each of its individual components in order to examine singular efficiency. 
+#This test allows our group to troubleshoot our application more effectively, thus allowing our group to single out each part that has problems instead of a holistic approach.
+############################################
+#Second, our group plans on gathering a group of three individual volunteers to engage in usability testing. 
+#This will consist of the volunteer being instructed to navigate to our website and attempt to use the features. 
+#Our goal is to observe and take note of what the initial intended purpose of our website is and 
+#further gather information on how easy it is for users to achieve their goal through using our application. This will allow our group to make necessary design 
+#changing and observe possible additions in a non-biased atmosphere.
+############################################
+#Third, our group will implement a python scripting automated testing feature that will populate our database with 
+#test information that we will then check for correctness of the database.
+#This will be done using true-false, it will return a list that contains feature names and status.
